581e2e1c7fb30013c1b1269c5f6fae29f138e3f1
Ajay Mahato <ajjayymahato@gmail.com>
2022-03-21 19:02:41 +0530

    module pushed

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Website Customisations',
+    'version': '1.0',
+    'category': 'Sale',
+    'summary': 'Improve Website Customisation',
+    'description': 'Improve Website Customisation',
+    'author': "Youngman",
+    'website': "http://www.yourcompany.com",
+    'company': 'Youngman',
+    'depends': ['website_sale'],
+    'data': [
+        'views/product_templates_views.xml',
+        'views/product_product_views.xml',
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+ }
